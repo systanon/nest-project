3ba24734979baac50a76fe45e2c49a5a39815de9
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2022-10-16 14:53:06 +0200

    fix(main): add app listen

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  const url = await app.getUrl();
+  console.log('🚀 ~ application running on url', url);
 }
 bootstrap();
