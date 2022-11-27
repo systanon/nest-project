d19a582b5e331349ca8692e43b4954aaa18906df
Yurii Musolov <yurii.musolov@gmail.com>
2022-11-27 15:53:45 +0200

    feat(main.ts): add api prefix 'api'

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
   const url = await app.getUrl();
   console.log('🚀 ~ application running on url', url);
