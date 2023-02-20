fd9e3fe4ce282398db9285d7adef6ceab2dc4dc1
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2023-02-20 18:22:45 +0100

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useWebSocketAdapter(new WsAdapter(app));
-  app.enableCors();
+  app.enableCors({ origin: true });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.use(cookieParser());
