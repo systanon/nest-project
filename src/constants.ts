771255345e49e8581e563f2691fbe7fe12b9b0ba
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2023-03-26 16:10:32 +0200

    chore: change max age to 2 days

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const PUBLIC = 'public';
 export const UPLOAD_DIRNAME = 'upload-files';
 export const UPLOAD_URL = `/files`;
 
-export const cookiesMaxAge = 60 * 60 * 24 * 14;
+export const cookiesMaxAge = 60 * 60 * 24 * 2;
